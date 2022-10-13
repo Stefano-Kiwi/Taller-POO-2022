7e98be7a70052f816eca0f61a16deba570f8528c
Stefano-Kiwi <stefanobudon@gmail.com>
2022-10-13 18:03:11 -0300

    Creo Regex para levantar datos

---
+++ src/main/java/inventario/Almacenamiento.java
@@ -0,0 +1,15 @@
+package inventario;
+
+import java.util.List;
+
+public class Almacenamiento {
+    private List<Obra> obras;
+
+    public Almacenamiento() {
+    }
+    
+    public void obtenerObras(String direccion){
+        
+        
+    }
+}
